c968ecc5e3727777031273b2680466ba2c7b9dfe
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2019-06-17 16:27:13 -0600

    Add files via upload

---
+++ TrafficAnimation.java
@@ -0,0 +1,311 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.text.StyleConstants.ColorConstants;
+
+/**
+ * CS 121 Project 1: Traffic Animation
+ *
+ * Animates a [Pirate ship sailing past a salty Sailor]
+ *
+ * @author BSU CS 121 Instructors
+ * @author [Robbie Gill]
+ */
+@SuppressWarnings("serial")
+public class TrafficAnimation extends JPanel
+{
+	// This is where you declare constants and variables that need to keep their
+	// values between calls	to paintComponent(). Any other variables should be
+	// declared locally, in the method where they are used.
+
+	/**
+	 * A constant to regulate the frequency of Timer events.
+	 * Note: 100ms is 10 frames per second - you should not need
+	 * a faster refresh rate than this
+	 */
+	private final int DELAY = 100; //milliseconds
+
+	/**
+	 * The anchor coordinate for drawing / animating. All of your vehicle's
+	 * coordinates should be relative to this offset value.
+	 */
+	private int xOffset = 0;
+	/**
+	 * The number of pixels added to xOffset each time paintComponent() is called.
+	 */
+	private int stepSize = 10;
+
+	private final Color BACKGROUND_COLOR = Color.white;
+
+	/* This method draws on the panel's Graphics context.
+	 * This is where the majority of your work will be.
+	 *
+	 * (non-Javadoc)
+	 * @see java.awt.Container#paint(java.awt.Graphics)
+	 */
+	public void paintComponent(Graphics g)
+	{
+		// Get the current width and height of the window.
+		int width = getWidth(); // panel width
+		int height = getHeight(); // panel height
+		
+		// Fill the graphics page with the background color.
+		g.setColor(BACKGROUND_COLOR);
+		g.fillRect(0, 0, width, height);
+
+		// Calculate the new xOffset position of the moving object.
+		xOffset  = (xOffset + stepSize) % width;
+		
+		// TODO: Use width, height, and xOffset to draw your scalable objects
+		// at their new positions on the screen
+		
+		//sky
+		Color skyBlue = new Color(56,176,222);
+		g.setColor(skyBlue);
+		g.fillRect(dimensionScalerX(0,width), dimensionScalerY(0,height), dimensionScalerX(620,width), dimensionScalerY(233,height));
+		
+		//sun
+		Color sunOrange = new Color(255,196,0);
+		g.setColor(sunOrange);
+		g.fillArc(dimensionScalerX(100,width), dimensionScalerY(0,height), dimensionScalerX(110,width), dimensionScalerY(117,height),180,360);
+		
+		//sea
+		g.setColor(Color.BLUE);
+		g.fillRect(dimensionScalerX(0,width), dimensionScalerY(167,height), dimensionScalerX(620,width), dimensionScalerY(133,height));
+		//ground
+		Color sand = new Color(255,255,196);
+		g.setColor(sand);
+		g.fillRect(dimensionScalerX(0,width), dimensionScalerY(280,height), dimensionScalerX(620,width), dimensionScalerY(133,height));
+		//cloud mid of screen
+		g.setColor(Color.white);
+		g.fillOval(dimensionScalerX(250,width), dimensionScalerY(33,height), dimensionScalerX(50,width), dimensionScalerY(33,height));
+		g.fillOval(dimensionScalerX(280,width), dimensionScalerY(33,height), dimensionScalerX(40,width), dimensionScalerY(27,height));
+		g.fillOval(dimensionScalerX(300,width), dimensionScalerY(37,height), dimensionScalerX(30,width), dimensionScalerY(20,height));
+		//cloud left corner of screen
+		g.setColor(Color.white);
+		g.fillOval(dimensionScalerX(100,width), dimensionScalerY(47,height), dimensionScalerX(50,width), dimensionScalerY(33,height));
+		g.fillOval(dimensionScalerX(130,width), dimensionScalerY(47,height), dimensionScalerX(40,width), dimensionScalerY(27,height));
+		g.fillOval(dimensionScalerX(160,width), dimensionScalerY(47,height), dimensionScalerX(30,width), dimensionScalerY(20,height));
+		
+		
+	
+		//Text in Window
+			
+		
+		
+		//Vehicle SHIP
+		
+		
+		//g.fillRect(xOffset + dimensionScalerX(,width), dimensionScalerY(,height), dimensionScalerX(,width), dimensionScalerY(,height));
+		
+		
+		
+		//main mast
+		Color hullBrown = new Color(65,41,6);
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(170,width), dimensionScalerY(33,height), dimensionScalerX(19,width), dimensionScalerY(167,height));
+		g.fillRect(xOffset + dimensionScalerX(120,width), dimensionScalerY(50,height), dimensionScalerX(100,width), dimensionScalerY(10,height));
+		//main sail
+		Color shipSail = new Color(240,234,213);
+		g.setColor(shipSail);
+		g.fillRect(xOffset + dimensionScalerX(120,width), dimensionScalerY(50,height), dimensionScalerX(100,width), dimensionScalerY(100,height));
+		//ships hull
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(0,width), dimensionScalerY(167,height), dimensionScalerX(300,width), dimensionScalerY(50,height));
+		
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(25,width), dimensionScalerY(200,height), dimensionScalerX(250,width), dimensionScalerY(33,height));
+		
+		g.setColor(Color.BLACK);
+		g.drawRect(xOffset + dimensionScalerX(0,width), dimensionScalerY(167,height), dimensionScalerX(300,width), dimensionScalerY(50,height));
+		
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(300,width), dimensionScalerY(167,height), dimensionScalerX(30,width), dimensionScalerY(33,height));
+		//front rig post
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(325,width), dimensionScalerY(160,height), dimensionScalerX(20,width), dimensionScalerY(20,height));
+		
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(340,width), dimensionScalerY(146,height), dimensionScalerX(15,width), dimensionScalerY(17,height));
+		
+		g.setColor(Color.BLACK);
+		g.drawRect(xOffset + dimensionScalerX(340,width), dimensionScalerY(147,height), dimensionScalerX(15,width), dimensionScalerY(17,height));
+		g.setColor(Color.BLACK);
+		g.drawRect(xOffset + dimensionScalerX(25,width), dimensionScalerY(167,height), dimensionScalerX(250,width), dimensionScalerY(10,height));
+		g.setColor(Color.BLACK);
+		g.drawRect(xOffset + dimensionScalerX(35,width), dimensionScalerY(177,height), dimensionScalerX(250,width), dimensionScalerY(10,height));
+		
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(350,width), dimensionScalerY(133,height), dimensionScalerX(30,width), dimensionScalerY(16,height));
+		//cabin
+		g.setColor(hullBrown);
+		g.fillRect(xOffset + dimensionScalerX(0,width), dimensionScalerY(135,height), dimensionScalerX(100,width), dimensionScalerY(33,height));
+		// windows.
+		g.setColor(Color.DARK_GRAY);
+		g.fillOval(xOffset + dimensionScalerX(250,width), dimensionScalerY(183,height), dimensionScalerX(17,width), dimensionScalerY(17,height));
+		g.fillOval(xOffset + dimensionScalerX(200,width), dimensionScalerY(183,height), dimensionScalerX(17,width), dimensionScalerY(17,height));
+		g.fillOval(xOffset + dimensionScalerX(150,width), dimensionScalerY(183,height), dimensionScalerX(17,width), dimensionScalerY(17,height));
+		g.fillOval(xOffset + dimensionScalerX(100,width), dimensionScalerY(2183,height), dimensionScalerX(17,width), dimensionScalerY(17,height));
+	
+		//Avatar
+		//pirates hat
+	/*This is a blue print of how the x,y values are oriented,
+	 * to form a hat on my avatar.
+	 * 			(x3,y3)*(x4,y4)*
+ * 			(x2,y2)* 		(x5,y5)* 
+	 * 	(x1,y1)*				(x6,y6)*
+ * 			(x10,y10)*		(x7,y7)*	
+	 * 			(x9,y9)*(x8,y8)*
+	 * 
+	 */
+		//X values pirate hat
+		int x1 = dimensionScalerX(300,width);
+		int x2 = dimensionScalerX(320,width);
+		int x3 = dimensionScalerX(340,width);
+		int x4 = dimensionScalerX(360,width);
+		int x5 = dimensionScalerX(380,width);
+		int x6 = dimensionScalerX(400,width);
+		int x7 = dimensionScalerX(380,width);
+		int x8 = dimensionScalerX(360,width);
+		int x9 = dimensionScalerX(340,width);
+		int x10 = dimensionScalerX(320,width);
+		
+		
+		//Y values pirate hat
+		int y1 = dimensionScalerY(350,height);
+		int y2 = dimensionScalerY(337,height);
+		int y3 = dimensionScalerY(320,height);
+		int y4 = dimensionScalerY(320,height);
+		int y5 = dimensionScalerY(337,height);
+		int y6 = dimensionScalerY(350,height);
+		int y7 = dimensionScalerY(362,height);
+		int y8 = dimensionScalerY(370,height);
+		int y9 = dimensionScalerY(370,height);
+		int y10 =dimensionScalerY(362,height);
+		int[] xPoints = {x1,x2,x3,x4,x5,x6,x7,x8,x9,x10};
+		int[] yPoints = {y1,y2,y3,y4,y5,y6,y7,y8,y9,y10}; 
+		g.setColor(Color.BLACK);
+		g.fillPolygon(xPoints, yPoints,10);
+	
+		//head
+		Color skinColor = new Color(255, 205, 148);
+		g.setColor(skinColor);
+		g.fillOval(dimensionScalerX(325,width), dimensionScalerY(343,height), dimensionScalerX(50,width), dimensionScalerY(57,height));
+		//eye
+		g.setColor(Color.white);
+		g.fillOval(dimensionScalerX(330,width), dimensionScalerY(355,height), dimensionScalerX(15,width), dimensionScalerY(15,height));
+		g.setColor(Color.blue);
+		g.fillOval(dimensionScalerX(333,width), dimensionScalerY(358,height), dimensionScalerX(10,width), dimensionScalerY(10,height));
+		//eye patch eye
+		g.setColor(Color.BLACK);
+		g.fillRect(dimensionScalerX(327,width), dimensionScalerY(355,height), dimensionScalerX(50,width), dimensionScalerY(4,height));
+		g.fillArc(dimensionScalerX(350,width), dimensionScalerY(343,height), dimensionScalerX(20,width), dimensionScalerY(27,height),180, 180);
+		
+		//mouth
+		g.setColor(Color.BLACK);
+		g.fillArc(dimensionScalerX(342,width), dimensionScalerY(372,height), dimensionScalerX(11,width), dimensionScalerY(11,height),360,360 );
+		//mustache
+		Color mustacheBrown = new Color(102,51,0);
+		g.setColor(mustacheBrown);
+		g.fillArc(dimensionScalerX(338,width), dimensionScalerY(370,height), dimensionScalerX(20,width), dimensionScalerY(14,height),340,210 );
+		// nose
+		g.setColor(skinColor);
+		g.fillRect(dimensionScalerX(345,width), dimensionScalerY(359,height),dimensionScalerX(5,width), dimensionScalerY(15,height));
+		g.setColor(Color.black);
+		g.drawRect(dimensionScalerX(345,width), dimensionScalerY(359,height),dimensionScalerX(5,width), dimensionScalerY(15,height));
+		
+		//
+		g.setColor(Color.RED);
+		g.setFont(new Font("TimesRoman",Font.PLAIN, dimensionScalerX(25,width)));
+		g.drawString("'YARGGGG!!'",dimensionScalerX(150,width), dimensionScalerY(359,height));
+		
+		// Put your code above this line. This makes the drawing smoother.
+		Toolkit.getDefaultToolkit().sync();
+	}
+
+	
+	//input desired x location and method getWidth() alias width.
+		public static int dimensionScalerX(int var ,int width) {
+			int result = ((width/6)*var)/100;
+			return (result);
+		};
+		//input desired Y location and method getHeight alias, height.
+		public static int dimensionScalerY(int var ,int height) {
+			int result = ((height/4)*var)/100;
+			return (result);
+		};
+	
+
+	//==============================================================
+	// You don't need to modify anything beyond this point.
+	//==============================================================
+
+
+	/**
+	 * Starting point for this program. Your code will not go in the
+	 * main method for this program. It will go in the paintComponent
+	 * method above.
+	 *
+	 * DO NOT MODIFY this method!
+	 *
+	 * @param args unused
+	 */
+	public static void main (String[] args)
+	{
+		// DO NOT MODIFY THIS CODE.
+		JFrame frame = new JFrame ("Traffic Animation");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new TrafficAnimation());
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Constructor for the display panel initializes necessary variables.
+	 * Only called once, when the program first begins. This method also
+	 * sets up a Timer that will call paint() with frequency specified by
+	 * the DELAY constant.
+	 */
+	public TrafficAnimation()
+	{
+		// Do not initialize larger than 800x600. I won't be able to
+		// grade your project if you do.
+		int initWidth = 600;
+		int initHeight = 400;
+		setPreferredSize(new Dimension(initWidth, initHeight));
+		this.setDoubleBuffered(true);
+
+		//Start the animation - DO NOT REMOVE
+		startAnimation();
+	}
+
+	/**
+	 * Create an animation thread that runs periodically.
+	 * DO NOT MODIFY this method!
+	 */
+	private void startAnimation()
+	{
+		ActionListener timerListener = new TimerListener();
+		Timer timer = new Timer(DELAY, timerListener);
+		timer.start();
+	}
+
+	/**
+	 * Repaints the graphics panel every time the timer fires.
+	 * DO NOT MODIFY this class!
+	 */
+	private class TimerListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) {
+			repaint();
+		}
+	}
+}  
